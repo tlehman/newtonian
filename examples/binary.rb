4748bec7254b43a421da8ded111a35d3251382cd
Tobi Lehman <mail@tobilehman.com>
2016-02-24 17:00:31 -0800

    Add canvas renderer

--- examples/binary.rb
+++ examples/binary.rb
@@ -6,8 +6,6 @@ require './lib/body'
 require './lib/universe'
 require 'json'
 
-module Newtonian
-
 dt = 0.001
 m = 10
 r = 1.0
@@ -21,11 +19,11 @@ body2 = Body.new(mass: m, position: body2_pos, velocity: [0,-v])
 
 universe = Universe.new(dimensions: 2, bodies: [body1, body2])
 
+i = 0
 loop do
   universe.evolve(dt)
   puts JSON.generate(universe.to_a)
   sleep dt
+  i += 1
 end
 
-
-end
